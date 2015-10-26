5e8a4426d6d1384ae8720595014ad8a21c67eaa5
Della Tettehklu <della.tettehklu@f1006-17-34178.bmcc.cuny.edu>
2015-10-26 15:56:53 -0400

    creating constructors for functions using bubbles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 //global variables
 
+
+
+//Bubble Constructor
+function Bubble(){
+    //create x and y properties (variables)
+    this.x = random(width);
+    this.y = random(height);
+    this.r = 20; //r is for radius which is 1/2 of the width of the circle.
+    this.bubbleSize = this.r;//width and height are twice the radius
+
+
+    //draw the bubble
+    ellipse(this.x,this.y,this.bubbleSize,this.bubbleSize);
+
+}// end of Bubble
+
 function setup(){
     //create the canvas. put into a variable to associate with a element
     var cnv = createCanvas(800,3000);
@@ -8,24 +24,21 @@ function setup(){
     var myArray = []; // create blank array
     //add stuff to the array with a loop
 
-    for(var i =0; i <10000; i++){
-        //myArray[i] = "item" + i;
-        myArray[i] =i;
+    for(var i =0; i <10; i++){
+
+        //addBubbles to the array
+        myArray[i] = new Bubble();
     }
     //log out the array
     console.log(myArray);
 
     //loop to go through every item in the array
         for( var i =0; i< myArray.length; i++){
-            myArray[i] +=10;
-            //console.log(myArray[i]);
+
+            //myArray[i] +=10;
+            console.log(myArray[i]);
         }
 
-    //console.log(myArray[0]);
-   // console.log(myArray[1]);
-    //console.log(myArray[2]);
-    //console.log(myArray);
-    //console.log("myArray", myArray);
 
     console.log(myArray);
 
